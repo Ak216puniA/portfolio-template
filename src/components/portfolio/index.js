9cbb6a94840a2fa6a9b08852a0ce715235a318a1
Akhil Punia <ak216punia@gmail.com>
2023-01-18 03:24:41 +0530

    Add user-info.js file

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -7,48 +7,10 @@ import NavigationBar from '../navigation-bar'
 import ProfilePic from '../profile-pic'
 import WelcomeMessage from '../welcome-message'
 import './index.css'
+import userInfo from '../../user-info'
 
 function Portfolio() {
-
-  const user = {
-    'greeting': 'Greeting-message',
-    'nameIntro': `Name-intro`,
-    'fieldOfInterest': {
-      'fieldOfInterest1': `Field-of-interest line1`,
-      'fieldOfInterest2': `Field-of-interest`,
-    },
-    'currentWorkPlace': {
-      'currentWorkPlace1': `Workplace line1`,
-      'currentWorkPlace2': `Workplace-name`
-    },
-    'quote': `Personal quote`,
-    'projects': [
-      {
-        'title': `Project-name`,
-        'desc': `Description`,
-        'link': `L-I-N-K`,
-      },
-      {
-        'title': `Project-name`,
-        'desc': `Description`,
-        'link': `L-I-N-K`,
-      }
-    ],
-    'about': [
-      `About para1`,
-      `About para2`,
-      `About paraN`
-    ],
-    'resume': `L-I-N-K`,
-    'contacts': {
-      'contactDesc': `Contact description`,
-      'facebook': `L-I-N-K`,
-      'linkedin': `L-I-N-K`,
-      'github': `L-I-N-K`,
-      'email': `L-I-N-K`
-    }
-  }
-  // Add your personal links in place of L-I-N-K
+  const user = userInfo
 
   useEffect(() => {
     var maxx = window.screen.width;
---
+++ src/user-info.js
@@ -0,0 +1,40 @@
+const userInfo = {
+    'greeting': 'Greeting-message',
+    'nameIntro': `Name-intro`,
+    'fieldOfInterest': {
+        'fieldOfInterest1': `Field-of-interest line1`,
+        'fieldOfInterest2': `Field-of-interest`,
+    },
+    'currentWorkPlace': {
+        'currentWorkPlace1': `Workplace line1`,
+        'currentWorkPlace2': `Workplace-name`
+    },
+    'quote': `Personal quote`,
+    'projects': [
+        {
+        'title': `Project-name`,
+        'desc': `Description`,
+        'link': `L-I-N-K`,
+        },
+        {
+        'title': `Project-name`,
+        'desc': `Description`,
+        'link': `L-I-N-K`,
+        }
+    ],
+    'about': [
+        `About para1`,
+        `About para2`,
+        `About paraN`
+    ],
+    'resume': `L-I-N-K`,
+    'contacts': {
+        'contactDesc': `Contact description`,
+        'facebook': `L-I-N-K`,
+        'linkedin': `L-I-N-K`,
+        'github': `L-I-N-K`,
+        'email': `L-I-N-K`
+    }
+}
+
+export default userInfo
